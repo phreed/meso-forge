5faf01f6d2d4c6b8f88d291adc9c66bbc1035ea0
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2025-03-10 17:25:50 -0500

    local build and promote
    repairs to packages

---
+++ .scripts/run_promote.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+    
+export ANACONDA_API_KEY=$(secret-tool lookup ANACONDA API_KEY)
+export ANACONDA_FORCE=true
+export ANACONDA_OWNER=mesomorph
+export ANACONDA_CHANNEL=dev
+export RATTLER_BUILD_LOG_STYLE=fancy
+
+for pkg in "$@"; do
+for conda in $(find output -type f \( -name "${pkg}*.conda" -o -name "${pkg}*.tar.bz2" \) ); do
+    echo "Uploading ${conda}"
+    rattler-build upload anaconda -v --force "${conda}"
+done
+done
